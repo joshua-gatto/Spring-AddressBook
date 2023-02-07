c4294b29a011726fed2d1dbe34badb1a7dc7fb66
joshua-gatto <joshuagatto@cmail.carleton.ca>
2023-02-07 14:39:13 -0500

    Fixed Controller.setBuddyInfoId() method implementation. Reverted changes in AddressBookRepository.java Added comments to .gitignore

--- src/main/java/org/lab1/AddressBookRepository.java
+++ src/main/java/org/lab1/AddressBookRepository.java
@@ -3,8 +3,6 @@ package org.lab1;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface AddressBookRepository extends CrudRepository<AddressBook, Long>{
 
--- src/main/java/org/lab1/Controller.java
+++ src/main/java/org/lab1/Controller.java
@@ -1,8 +1,12 @@
 package org.lab1;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.*;;
 
+/**
+ * The Controller class is a RESTful web service class used to handle incoming HTTP requests. It handles the functionality
+ * of creating and modifying BuddyInfo objects and AddressBook objects.
+ * @Author Joshua Gatto
+ */
 @RestController
 public class Controller {
 
@@ -21,9 +25,9 @@ public class Controller {
     }
 
     //BuddyInfo Controller
-
+        //Constructor
     /**
-     * Creates a new BuddyInfo. The id attribute is automatically generated and assigned.
+     * Main constructor for BuddyInfo. The id attribute is automatically generated and assigned.
      * @param phoneNo Buddy's phone number
      * @param name Buddy's name
      * @return new BuddyInfo object
@@ -34,19 +38,17 @@ public class Controller {
     }
 
     /**
-     * Reassigns a BuddyInfo an ID. The id attribute has already been set when the BuddyInfo object was initialized.
-     * @param id Identification number that the BuddyInfo can be referenced by
-     * @param buddy The BuddyInfo to apply the reassignment to
+     * Retrieve a BuddyInfo by identification number from the repository
+     * @param id identification number of the desired BuddyInfo
+     * @return
      */
-    @PutMapping("setId/{buddy}/{id}")
-    public BuddyInfo addBuddyInfo(@PathVariable Long id, @RequestBody BuddyInfo buddy){
-        buddy.setId(id);
-        return buddyInfoRepository.save(buddy);
+    @GetMapping("getBuddy/{id}")
+    public BuddyInfo getBuddyInfo(@PathVariable Long id){
+        return buddyInfoRepository.findById(id).orElse(null);
     }
 
-    //AddressBook Controller
     /**
-     * Retrieves the list of BuddyInfos in the AddressBook.
+     * Retrieves the list of all BuddyInfos in the repository.
      * @return Iterable BuddyInfo collection
      */
     @GetMapping("buddies")
@@ -54,6 +56,40 @@ public class Controller {
         return buddyInfoRepository.findAll();
     }
 
+    /**
+     * Reassigns a BuddyInfo an id. Any AddressBooks that contain the specified BuddyInfo will have it removed before
+     * it is updated and added again. The id attribute has already been set when the BuddyInfo object was initialized.
+     * @param oldId The BuddyInfo to change the id of
+     * @param newId The new identification number of the BuddyInfo
+     */
+    @PutMapping("setId/{oldId}/{newId}")
+    public BuddyInfo setBuddyInfoId(@PathVariable Long oldId, @PathVariable Long newId){
+        BuddyInfo buddy = getBuddyInfo(oldId);
+        if(buddy != null){ //checks if buddy retrieval was successful
+            AddressBook[] fromBook = new AddressBook[(int)addressBookRepository.count()]; //empty array to store AddressBooks that contain specified BuddyInfo
+            int i = 0;
+            for(AddressBook addressBook : addressBookRepository.findAll()){ //iterating over all AddressBooks in repository
+                if(addressBook.containsBuddy(buddy)){ //checking if the BuddyInfo is in the AddressBook
+                    addressBook.removeBuddy(buddy); //remove the BuddyInfo
+                    fromBook[i] = addressBook; //Add AddressBook to array
+                    addressBookRepository.save(addressBook); //update the AddressBook's repository
+                    i++;
+                }
+            }
+            buddyInfoRepository.delete(buddy); //deleting old BuddyInfo
+            buddy.setId(newId); //update id attribute
+            buddyInfoRepository.save(buddy); //add BuddyInfo to repository
+            for(AddressBook addressBook : fromBook){
+                addressBook.addBuddy(buddy);
+                addressBookRepository.save(addressBook);
+            }
+            return buddy;
+        }else{
+            return null;
+        }
+    }
+
+    //AddressBook Controller
     /**
      * Adds a BuddyInfo to an AddressBook.
      * @param addressBookId Identification number of the AddressBook
